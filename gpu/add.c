1122e987ab46cfa2f219df423f5371391b7c4fab
shaoguangleo <shaoguangleo@gmail.com>
2016-05-19 12:04:16 +0800

    Add simple add function

--- gpu/add.c
+++ gpu/add.c
@@ -1,4 +1,31 @@
-//
-// Created by leo on 16/5/18.
-//
+/*
+ * @filename : add.c
+ */
 
+#include <iostream>
+#include "book.h"
+
+__global__ void add(int a, int b, int *c)
+{
+    *c = a + b;
+}
+
+int main(void)
+{
+    int c;
+    int *dev_c;
+    /*
+     * Will do malloc when has operation
+     */
+    HANDEL_ERROR(cudaMalloc((void**)&dev_c, sizeof(int)));
+
+    add<<<1,1>>>(2,7,dev_c);
+
+    HANDEL_ERROR(cudaMemcpy(&c,dev_c, sizeof(int),cudaMemcpyDevideToHost));
+
+    printf("2 + 7 = %d\n",c);
+
+    cudaFree(dev_c);
+
+    return 0;
+}
