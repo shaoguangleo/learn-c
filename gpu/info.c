ccf77802322475e3545478782dd66ca0ad00aeb4
shaoguangleo <shaoguangleo@gmail.com>
2016-05-19 12:04:28 +0800

    Add simple info function

--- gpu/info.c
+++ gpu/info.c
@@ -1,4 +1,29 @@
-//
-// Created by leo on 16/5/19.
-//
+#include "book.h"
+#include <stdio.h>
+#include "cuda.h"
 
+int main(void)
+{
+    cudaDeviceProp prop;
+
+    int count;
+
+    HANDLE_ERROR( cudaGetDeviceCount (&count));
+
+    for (int i = 0;i<count; i++)
+    {
+        HANDLE_ERROR(cudaGetDeviceProperties(&prop,i));
+        /*
+         * Do other operation on the properties of GPU
+         */
+        printf("   --- General Information for device %d ---",i);
+        printf("Name: %s\n",prop.name);
+        printf("Computer capability: %d.%d\n",prop.major,prop.minor);
+        printf("Clock rate : %d\n",prop.clockRate);
+        printf("Device copy overlap: ");
+        if (prop.deviceOverlap)
+            printf("Enabled\n");
+        else
+            printf("Disabled\n");
+    }
+}
